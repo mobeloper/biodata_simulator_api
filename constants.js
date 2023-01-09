4faa5665876863c977313b60a257732690c3c601
koder <frack52@hotmail.com>
2023-01-09 11:05:33 +0900

    [modified] updated ranges

--- constants.js
+++ constants.js
@@ -1,30 +1,27 @@
 
-// range to generate mock data
+// Normal ranges to generate mock data
 
 //Normal-> 60 - 100
 const heartBeatMin = 30 //40
 const heartBeatMax =120
 
 // Normal -> 360 - 600 minutes
-
 const sleepDurationMin = 240
 const sleepDurationMax = 720
 
 const stepsWalkedMin = 100
 const stepsWalkedMax=10000
 
-// Normal --> 70 - 140 mg/dL
 
+// Normal --> 70 - 140 mg/dL
 const glucoseLevelMin = 60
 const glucoseLevelMax = 200
 
 // Normal Below 120
-
 const bpMin = 60
 const bpMax = 180
 
 // Normal -> 96 - 98 %
-
 const oxygenSaturationMin = 93
 const oxygenSaturationMax = 98
 
@@ -34,8 +31,5 @@ function generate(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); 
   }
-
-
-
 
 module.exports = { heartBeatMin,heartBeatMax,sleepDurationMin,sleepDurationMax,stepsWalkedMin,stepsWalkedMax,glucoseLevelMin,glucoseLevelMax,bpMin,bpMax,oxygenSaturationMin,oxygenSaturationMax,generate}
