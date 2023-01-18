1ba6a49c7d94eaa86051f1edb85cebd69367b0a3
koder <frack52@hotmail.com>
2023-01-19 07:53:15 +0900

    [update] pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'server',
-        script: './server.js',
+        script: 'server.js',
         instances: 0,
         exec_mode: 'cluster',
       },
