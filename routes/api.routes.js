50fc1ec52f5e32f67c174b80cf033abd6368a06e
Koder <frack52@hotmail.com>
2024-04-03 03:54:56 +0900

    Update api.routes.js

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,18 +1,3 @@
-//var router = require("express").Router();
-
-//import { Application } from "express";
-
-//const healthAPI = require("../controllers/health.controller.js");
-
-// module.exports = app => {
-  
-//     router.get("/health/all/v1",healthAPI.all);
-//     router.get("/health/activity/v1",healthAPI.activity);
-
-//     //API root path
-//     app.use("/api", router);
-// };
-
 import Router from "express-promise-router";
 
 import {allDummy, authLogin, getActivity, getSleep, receiveHealthDataFromMetriport, authUserInfo} from "../controllers/health.controller.js"
@@ -20,11 +5,6 @@ import {allDummy, authLogin, getActivity, getSleep, receiveHealthDataFromMetripo
 const router = Router();
 
 export default app => {
-//export default (app: Application) => {
-
-    // app.use("/health/mock/v1",allDummy);
-    // app.use("/health/activity/v1",getActivity);
-    // app.use("/health/sleep/v1",getSleep);
 
     router.get("/auth/login/v1",authLogin);
     router.get("/auth/userInfo/v1",authUserInfo);
@@ -33,8 +13,6 @@ export default app => {
     router.get("/health/activity/v1",getActivity);
     router.get("/health/sleep/v1",getSleep);
     
-    
-
     router.post("/health/metriport/v1", receiveHealthDataFromMetriport);
 
     // //API root path
